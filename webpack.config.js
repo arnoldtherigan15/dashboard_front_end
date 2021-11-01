79ca15df08d56cd5ed5562fe43e50b1b5d330d22
arnoldtherigan15 <arnoldtherigan15@gmail.com>
2021-11-01 19:03:53 +0700

    initialize repository

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+let mode = "development"
+
+if(process.env.NODE_ENV == "production") mode = "production"
+
+module.exports = {
+    mode, 
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "index.bundle.js"
+    },
+    devServer: {
+        port: 8080,
+        // watchContentBase: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g)$/i,
+                type: "asset/resource"
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "/src/public/index.html")
+        }),
+        new CopyWebpackPlugin({
+            patterns: [ 
+                {
+                    from:  path.join(__dirname, "/src/public/img"),
+                    to: 'img'
+                },
+            ]
+        }),
+    ],
+    resolve: {
+        extensions: [".js",".jsx"]
+    }
+}
