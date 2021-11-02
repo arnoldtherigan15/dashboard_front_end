60643e2f253e86a28d904303a0045340de681daa
arnoldtherigan15 <arnoldtherigan15@gmail.com>
2021-11-02 20:20:12 +0700

    area chart initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { AreaChart } from './components'
+import { Navbar } from './components'
 import './styles/index.css'
 export default function App() {
     return (
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,8 @@
+import './index.css'
+export default function Navbar() {
+    return (
+        <div>
+            <h1>Navbar nih</h1>
+        </div>
+    )
+}
